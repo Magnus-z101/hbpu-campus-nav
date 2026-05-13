2fd6eeba5d820ca56bb295d1692a48a29343ed67
User <user@example.com>
2026-05-13 12:07:26 +0800

    Add missing @radix-ui/react-label dependency

---
+++ _next/static/wDSRuaz869trm2ExCCtqk/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hbpu-campus-nav//_next/:path+",
+        "destination": "/hbpu-campus-nav/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
